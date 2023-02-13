0efeb9d1a15a06d7c403f34d373d491d7839aca8
Loïc Greffier <loic.greffier_ext@michelin.com>
2023-02-13 10:34:23 +0100

    Initial commit

---
+++ src/main/java/com/michelin/kafka/streams/utils/KafkaStreamsUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.michelin.kafka.streams.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsUtilsApplication.class, args);
+	}
+
+}
