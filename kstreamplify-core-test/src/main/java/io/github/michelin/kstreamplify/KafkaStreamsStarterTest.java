8d7422945e39068e6487df3d4a5148beac4ce01c
FP17639 <clement.vacher_ext@michelin.com>
2023-07-02 12:53:44 +0200

    🧪 Add TopicWithSerde helper method in StarterTest class

--- kstreamplify-core-test/src/main/java/io/github/michelin/kstreamplify/KafkaStreamsStarterTest.java
+++ kstreamplify-core-test/src/main/java/io/github/michelin/kstreamplify/KafkaStreamsStarterTest.java
@@ -3,9 +3,8 @@ package io.github.michelin.kstreamplify;
 import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.github.michelin.kstreamplify.context.KafkaStreamsExecutionContext;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.TopologyTestDriver;
+import io.github.michelin.kstreamplify.utils.TopicWithSerde;
+import org.apache.kafka.streams.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -75,5 +74,13 @@ public abstract class KafkaStreamsStarterTest {
         testDriver.close();
         Files.deleteIfExists(Paths.get(STATE_DIR));
         MockSchemaRegistry.dropScope("mock://" + getClass().getName());
+    }
+    
+    protected <K, V> TestInputTopic<K, V> createInputTestTopic(TopicWithSerde<K, V> topicWithSerde) {
+        return this.testDriver.createInputTopic(topicWithSerde.getUnPrefixedName(), topicWithSerde.getKeySerde().serializer(), topicWithSerde.getValueSerde().serializer());
+    }
+
+    protected <K, V> TestOutputTopic<K, V> createOutputTestTopic(TopicWithSerde<K, V> topicWithSerde) {
+        return this.testDriver.createOutputTopic(topicWithSerde.getUnPrefixedName(), topicWithSerde.getKeySerde().deserializer(), topicWithSerde.getValueSerde().deserializer());
     }
 }
