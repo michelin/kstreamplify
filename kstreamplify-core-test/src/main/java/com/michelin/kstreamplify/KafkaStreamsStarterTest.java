cbffc716e8fc6bab4e77524ef8ee852c30fdadb8
clvacher <clement.vacher@hotmail.fr>
2023-08-03 13:25:48 +0200

    ⛔ close #71 dlq topic definition in tests (#73)
    * ⛔ close #71 dlq topic definition in tests
    
    * Add default dlqTopic test output topic in tests and force dlq definition in KafkaStreamsStarter

--- kstreamplify-core-test/src/main/java/com/michelin/kstreamplify/KafkaStreamsStarterTest.java
+++ kstreamplify-core-test/src/main/java/com/michelin/kstreamplify/KafkaStreamsStarterTest.java
@@ -1,9 +1,14 @@
 package com.michelin.kstreamplify;
 
+import com.michelin.kstreamplify.avro.KafkaError;
 import com.michelin.kstreamplify.context.KafkaStreamsExecutionContext;
+import com.michelin.kstreamplify.initializer.KafkaStreamsStarter;
+import com.michelin.kstreamplify.utils.SerdesUtils;
 import com.michelin.kstreamplify.utils.TopicWithSerde;
 import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.streams.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,20 +21,21 @@ import java.util.Collections;
 import java.util.Properties;
 
 /**
- * The main test class to extend to execute unit tests on topology
+ * <p>The main test class to extend to execute unit tests on topology</p>
+ * <p>It provides a {@link TopologyTestDriver} and a {@link TestOutputTopic} for the DLQ</p>
  */
 public abstract class KafkaStreamsStarterTest {
     private static final String STATE_DIR = "/tmp/kafka-streams";
 
     /**
-     * The default DLQ topic name used for testing
-     */
-    protected static final String DLQ_TOPIC = "DLQ_TOPIC";
-
-    /**
      * The topology test driver
      */
     protected TopologyTestDriver testDriver;
+
+    /**
+     * The dlq topic, initialized in {@link #generalSetUp()}
+     */
+    protected TestOutputTopic<String, KafkaError> dlqTopic;
 
     /**
      * Set up topology test driver
@@ -42,24 +48,31 @@ public abstract class KafkaStreamsStarterTest {
         properties.setProperty(StreamsConfig.STATE_DIR_CONFIG, STATE_DIR);
 
         KafkaStreamsExecutionContext.registerProperties(properties);
-        KafkaStreamsExecutionContext.setDlqTopicName(DLQ_TOPIC);
         KafkaStreamsExecutionContext.setSerdesConfig(Collections
                 .singletonMap(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "mock://" + getClass().getName()));
 
+        var starter = getKafkaStreamsStarter();
+
+        KafkaStreamsExecutionContext.setDlqTopicName(starter.dlqTopic());
+
         StreamsBuilder streamsBuilder = new StreamsBuilder();
-        topology(streamsBuilder);
+        starter.topology(streamsBuilder);
 
         testDriver = new TopologyTestDriver(streamsBuilder.build(), properties, getInitialWallClockTime());
+
+        dlqTopic = testDriver.createOutputTopic(KafkaStreamsExecutionContext.getDlqTopicName(), new StringDeserializer(), SerdesUtils.<KafkaError>getSerdesForValue().deserializer());
     }
 
     /**
-     * Method to create the topology to test
-     * @param streamsBuilder to build the topology of the stream
+     * Method to override to provide the KafkaStreamsStarter to test
+     *
+     * @return The KafkaStreamsStarter to test
      */
-    protected abstract void topology(StreamsBuilder streamsBuilder);
+    protected abstract KafkaStreamsStarter getKafkaStreamsStarter();
 
     /**
      * Default base wall clock time for topology test driver
+     *
      * @return The default wall clock time as instant
      */
     protected Instant getInitialWallClockTime() {
@@ -78,21 +91,23 @@ public abstract class KafkaStreamsStarterTest {
 
     /**
      * Creates an input test topic on the testDriver using the provided topicWithSerde
+     *
      * @param topicWithSerde The topic with serde used to crete the test topic
+     * @param <K>            The serializable type of the key
+     * @param <V>            The serializable type of the value
      * @return The corresponding TestInputTopic
-     * @param <K> The serializable type of the key
-     * @param <V> The serializable type of the value
      */
     protected <K, V> TestInputTopic<K, V> createInputTestTopic(TopicWithSerde<K, V> topicWithSerde) {
         return this.testDriver.createInputTopic(topicWithSerde.getUnPrefixedName(), topicWithSerde.getKeySerde().serializer(), topicWithSerde.getValueSerde().serializer());
     }
-    
+
     /**
      * Creates an output test topic on the testDriver using the provided topicWithSerde
+     *
      * @param topicWithSerde The topic with serde used to crete the test topic
+     * @param <K>            The serializable type of the key
+     * @param <V>            The serializable type of the value
      * @return The corresponding TestOutputTopic
-     * @param <K> The serializable type of the key
-     * @param <V> The serializable type of the value
      */
     protected <K, V> TestOutputTopic<K, V> createOutputTestTopic(TopicWithSerde<K, V> topicWithSerde) {
         return this.testDriver.createOutputTopic(topicWithSerde.getUnPrefixedName(), topicWithSerde.getKeySerde().deserializer(), topicWithSerde.getValueSerde().deserializer());
