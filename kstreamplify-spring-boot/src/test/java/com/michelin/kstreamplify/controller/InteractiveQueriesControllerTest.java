617fe5b02817c6ce559784fc33f61577153ef31a
Loïc Greffier <loic.greffier@michelin.com>
2024-08-04 12:29:49 +0200

    Sonar

--- kstreamplify-spring-boot/src/test/java/com/michelin/kstreamplify/controller/InteractiveQueriesControllerTest.java
+++ kstreamplify-spring-boot/src/test/java/com/michelin/kstreamplify/controller/InteractiveQueriesControllerTest.java
@@ -3,7 +3,6 @@ package com.michelin.kstreamplify.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import com.michelin.kstreamplify.service.InteractiveQueriesService;
