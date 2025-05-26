28c936dd555f07ba75976e3810fd2745c0609a2f
Loïc Greffier <loic.greffier@michelin.com>
2025-05-26 21:40:57 +0200

    Reduce code duplication for SerdesUtils

--- kstreamplify-core/src/main/java/com/michelin/kstreamplify/utils/SerdesUtils.java
+++ kstreamplify-core/src/main/java/com/michelin/kstreamplify/utils/SerdesUtils.java
@@ -18,7 +18,6 @@
  */
 package com.michelin.kstreamplify.utils;
 
-import com.michelin.kstreamplify.context.KafkaStreamsExecutionContext;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.avro.specific.SpecificRecord;
 
@@ -38,7 +37,7 @@ public final class SerdesUtils {
      * @return a serdes for requested class
      */
     public static <T extends SpecificRecord> SpecificAvroSerde<T> getSerdesForKey() {
-        return getSerdes(true);
+        return com.michelin.kstreamplify.serde.SerdesUtils.getKeySerdes();
     }
 
     /**
@@ -48,19 +47,6 @@ public final class SerdesUtils {
      * @return a serdes for requested class
      */
     public static <T extends SpecificRecord> SpecificAvroSerde<T> getSerdesForValue() {
-        return getSerdes(false);
-    }
-
-    /**
-     * Return a serdes for a requested class.
-     *
-     * @param isSerdeForKey Is the serdes for a key or a value
-     * @param <T> The class of requested serdes
-     * @return a serdes for requested class
-     */
-    private static <T extends SpecificRecord> SpecificAvroSerde<T> getSerdes(boolean isSerdeForKey) {
-        SpecificAvroSerde<T> serde = new SpecificAvroSerde<>();
-        serde.configure(KafkaStreamsExecutionContext.getSerdesConfig(), isSerdeForKey);
-        return serde;
+        return com.michelin.kstreamplify.serde.SerdesUtils.getValueSerdes();
     }
 }
