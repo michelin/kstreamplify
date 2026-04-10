554354c275fb5b32a0671a6b63c2160c08e32cfc
Sebastien Viale <sebastien.viale@michelin.com>
2026-04-10 14:09:25 +0200

    add processor name for TopicWithSerde topology builder

--- kstreamplify-core/src/test/java/com/michelin/kstreamplify/serde/TopicWithSerdeTest.java
+++ kstreamplify-core/src/test/java/com/michelin/kstreamplify/serde/TopicWithSerdeTest.java
@@ -75,12 +75,30 @@ class TopicWithSerdeTest {
         topicWithSerde.stream(streamsBuilder);
 
         assertEquals("""
-            Topologies:
-               Sub-topology: 0
-                Source: KSTREAM-SOURCE-0000000000 (topics: [INPUT_TOPIC])
-                  --> none
+                Topologies:
+                   Sub-topology: 0
+                    Source: KSTREAM-SOURCE-0000000000 (topics: [INPUT_TOPIC])
+                      --> none
 
-            """, streamsBuilder.build().describe().toString());
+                """, streamsBuilder.build().describe().toString());
+    }
+
+    @Test
+    void shouldCreateStreamWithName() {
+        KafkaStreamsExecutionContext.registerProperties(new Properties());
+
+        TopicWithSerde<String, String> topicWithSerde =
+                new TopicWithSerde<>("INPUT_TOPIC", Serdes.String(), Serdes.String());
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        topicWithSerde.stream(streamsBuilder, "consumer-name");
+        assertEquals("""
+                Topologies:
+                   Sub-topology: 0
+                    Source: consumer-name (topics: [INPUT_TOPIC])
+                      --> none
+
+                """, streamsBuilder.build().describe().toString());
     }
 
     @Test
@@ -94,15 +112,37 @@ class TopicWithSerdeTest {
         topicWithSerde.table(streamsBuilder, "myStore");
 
         assertEquals("""
-            Topologies:
-               Sub-topology: 0
-                Source: KSTREAM-SOURCE-0000000000 (topics: [INPUT_TOPIC])
-                  --> KTABLE-SOURCE-0000000001
-                Processor: KTABLE-SOURCE-0000000001 (stores: [myStore])
-                  --> none
-                  <-- KSTREAM-SOURCE-0000000000
+                Topologies:
+                   Sub-topology: 0
+                    Source: KSTREAM-SOURCE-0000000000 (topics: [INPUT_TOPIC])
+                      --> KTABLE-SOURCE-0000000001
+                    Processor: KTABLE-SOURCE-0000000001 (stores: [myStore])
+                      --> none
+                      <-- KSTREAM-SOURCE-0000000000
 
-            """, streamsBuilder.build().describe().toString());
+                """, streamsBuilder.build().describe().toString());
+    }
+
+    @Test
+    void shouldCreateTableWithName() {
+        KafkaStreamsExecutionContext.registerProperties(new Properties());
+
+        TopicWithSerde<String, String> topicWithSerde =
+                new TopicWithSerde<>("INPUT_TOPIC", Serdes.String(), Serdes.String());
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        topicWithSerde.table(streamsBuilder, "myStore", "table-processor-name");
+
+        assertEquals("""
+                Topologies:
+                   Sub-topology: 0
+                    Source: table-processor-name-source (topics: [INPUT_TOPIC])
+                      --> table-processor-name
+                    Processor: table-processor-name (stores: [myStore])
+                      --> none
+                      <-- table-processor-name-source
+
+                """, streamsBuilder.build().describe().toString());
     }
 
     @Test
@@ -116,14 +156,36 @@ class TopicWithSerdeTest {
         topicWithSerde.globalTable(streamsBuilder, "myStore");
 
         assertEquals("""
-            Topologies:
-               Sub-topology: 0 for global store (will not generate tasks)
-                Source: KSTREAM-SOURCE-0000000000 (topics: [INPUT_TOPIC])
-                  --> KTABLE-SOURCE-0000000001
-                Processor: KTABLE-SOURCE-0000000001 (stores: [myStore])
-                  --> none
-                  <-- KSTREAM-SOURCE-0000000000
-            """, streamsBuilder.build().describe().toString());
+                Topologies:
+                   Sub-topology: 0 for global store (will not generate tasks)
+                    Source: KSTREAM-SOURCE-0000000000 (topics: [INPUT_TOPIC])
+                      --> KTABLE-SOURCE-0000000001
+                    Processor: KTABLE-SOURCE-0000000001 (stores: [myStore])
+                      --> none
+                      <-- KSTREAM-SOURCE-0000000000
+                """, streamsBuilder.build().describe().toString());
+    }
+
+    @Test
+    void shouldCreateGlobalKtableWithName() {
+        KafkaStreamsExecutionContext.registerProperties(new Properties());
+
+        TopicWithSerde<String, String> topicWithSerde =
+                new TopicWithSerde<>("INPUT_TOPIC", Serdes.String(), Serdes.String());
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        topicWithSerde.globalTable(streamsBuilder, "myStore", "global-table-processor-name");
+        System.out.println(streamsBuilder.build().describe());
+
+        assertEquals("""
+                Topologies:
+                   Sub-topology: 0 for global store (will not generate tasks)
+                    Source: global-table-processor-name-source (topics: [INPUT_TOPIC])
+                      --> global-table-processor-name
+                    Processor: global-table-processor-name (stores: [myStore])
+                      --> none
+                      <-- global-table-processor-name-source
+                """, streamsBuilder.build().describe().toString());
     }
 
     @Test
@@ -153,20 +215,65 @@ class TopicWithSerdeTest {
         topicUserWithSerde.addGlobalStore(streamsBuilder, timestampedStoreBuilder, MyUserProcessor::new);
 
         assertEquals("""
-            Topologies:
-               Sub-topology: 0 for global store (will not generate tasks)
-                Source: KSTREAM-SOURCE-0000000000 (topics: [INPUT_TOPIC])
-                  --> KTABLE-SOURCE-0000000001
-                Processor: KTABLE-SOURCE-0000000001 (stores: [myStore])
-                  --> none
-                  <-- KSTREAM-SOURCE-0000000000
-              Sub-topology: 1 for global store (will not generate tasks)
-                Source: KSTREAM-SOURCE-0000000002 (topics: [INPUT_USER_TOPIC])
-                  --> KTABLE-SOURCE-0000000003
-                Processor: KTABLE-SOURCE-0000000003 (stores: [myTimestampedStore])
-                  --> none
-                  <-- KSTREAM-SOURCE-0000000002
-            """, streamsBuilder.build().describe().toString());
+                Topologies:
+                   Sub-topology: 0 for global store (will not generate tasks)
+                    Source: KSTREAM-SOURCE-0000000000 (topics: [INPUT_TOPIC])
+                      --> KTABLE-SOURCE-0000000001
+                    Processor: KTABLE-SOURCE-0000000001 (stores: [myStore])
+                      --> none
+                      <-- KSTREAM-SOURCE-0000000000
+                  Sub-topology: 1 for global store (will not generate tasks)
+                    Source: KSTREAM-SOURCE-0000000002 (topics: [INPUT_USER_TOPIC])
+                      --> KTABLE-SOURCE-0000000003
+                    Processor: KTABLE-SOURCE-0000000003 (stores: [myTimestampedStore])
+                      --> none
+                      <-- KSTREAM-SOURCE-0000000002
+                """, streamsBuilder.build().describe().toString());
+    }
+
+    @Test
+    void shouldCreateGlobalStoreWithName() {
+        KafkaStreamsExecutionContext.registerProperties(new Properties());
+        KafkaStreamsExecutionContext.setSerdesConfig(
+                Collections.singletonMap(SCHEMA_REGISTRY_URL_CONFIG, "http://mock:8081"));
+
+        TopicWithSerde<String, String> topicWithSerde =
+                new TopicWithSerde<>("INPUT_TOPIC", Serdes.String(), Serdes.String());
+
+        TopicWithSerde<String, KafkaUserStub> topicUserWithSerde =
+                new TopicWithSerde<>("INPUT_USER_TOPIC", Serdes.String(), SerdesUtils.getValueSerdes());
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        StoreBuilder<KeyValueStore<String, String>> storeBuilder = Stores.keyValueStoreBuilder(
+                Stores.persistentKeyValueStore("myStore"), Serdes.String(), Serdes.String());
+
+        StoreBuilder<TimestampedKeyValueStore<String, KafkaUserStub>> timestampedStoreBuilder =
+                Stores.timestampedKeyValueStoreBuilder(
+                        Stores.persistentTimestampedKeyValueStore("myTimestampedStore"),
+                        Serdes.String(),
+                        SerdesUtils.getValueSerdes());
+
+        topicWithSerde.addGlobalStore(
+                streamsBuilder, storeBuilder, MyStringProcessor::new, "kv-global-store-processor");
+        topicUserWithSerde.addGlobalStore(
+                streamsBuilder, timestampedStoreBuilder, MyUserProcessor::new, "timestamped-kv-global-store-processor");
+        System.out.println(streamsBuilder.build().describe());
+        assertEquals("""
+                Topologies:
+                   Sub-topology: 0 for global store (will not generate tasks)
+                    Source: kv-global-store-processor-source (topics: [INPUT_TOPIC])
+                      --> kv-global-store-processor
+                    Processor: kv-global-store-processor (stores: [myStore])
+                      --> none
+                      <-- kv-global-store-processor-source
+                  Sub-topology: 1 for global store (will not generate tasks)
+                    Source: timestamped-kv-global-store-processor-source (topics: [INPUT_USER_TOPIC])
+                      --> timestamped-kv-global-store-processor
+                    Processor: timestamped-kv-global-store-processor (stores: [myTimestampedStore])
+                      --> none
+                      <-- timestamped-kv-global-store-processor-source
+                """, streamsBuilder.build().describe().toString());
     }
 
     private static class MyStringProcessor extends ContextualProcessor<String, String, Void, Void> {
