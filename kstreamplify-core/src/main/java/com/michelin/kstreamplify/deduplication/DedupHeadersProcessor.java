649af8226c3f8cbe61a1799a65f994123471d6ce
Sebastien Viale <sebastien.viale@michelin.com>
2026-04-14 16:09:35 +0200

    Header-based deduplication support (#510)
    * add deduplication based on headers
    
    * add deduplication based on headers
    
    fix java doc

---
+++ kstreamplify-core/src/main/java/com/michelin/kstreamplify/deduplication/DedupHeadersProcessor.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.michelin.kstreamplify.deduplication;
+
+import com.michelin.kstreamplify.error.ProcessingResult;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.avro.specific.SpecificRecord;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.streams.processor.api.Processor;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Record;
+import org.apache.kafka.streams.state.WindowStore;
+
+/**
+ * Transformer class for the deduplication mechanism on headers of a given topic.
+ *
+ * @param <V> The type of the value
+ */
+public class DedupHeadersProcessor<V extends SpecificRecord>
+        implements Processor<String, V, String, ProcessingResult<V, V>> {
+
+    /** Window store name, initialized @ construction. */
+    private final String windowStoreName;
+    /** Retention window for the state store. Used for fetching data. */
+    private final Duration retentionWindowDuration;
+    /** Deduplication headers list. */
+    private final List<String> deduplicationHeadersList;
+    /** Kstream context for this transformer. */
+    private ProcessorContext<String, ProcessingResult<V, V>> processorContext;
+    /** Window store containing all the records seen on the given window. */
+    private WindowStore<String, String> dedupWindowStore;
+
+    /**
+     * Constructor.
+     *
+     * @param windowStoreName Name of the deduplication state store
+     * @param retentionWindowDuration Retention window duration
+     * @param deduplicationHeadersList Deduplication headers list
+     */
+    public DedupHeadersProcessor(
+            String windowStoreName, Duration retentionWindowDuration, List<String> deduplicationHeadersList) {
+        this.windowStoreName = windowStoreName;
+        this.retentionWindowDuration = retentionWindowDuration;
+        this.deduplicationHeadersList = deduplicationHeadersList;
+    }
+
+    @Override
+    public void init(ProcessorContext<String, ProcessingResult<V, V>> context) {
+        this.processorContext = context;
+        dedupWindowStore = this.processorContext.getStateStore(windowStoreName);
+    }
+
+    @Override
+    public void process(Record<String, V> message) {
+        try {
+            // Get the record timestamp
+            var currentInstant = Instant.ofEpochMilli(message.timestamp());
+            String identifier = buildIdentifier(message.headers());
+
+            // Retrieve all the matching keys in the stateStore and return null if found it (signaling a duplicate)
+            try (var resultIterator = dedupWindowStore.backwardFetch(
+                    identifier,
+                    currentInstant.minus(retentionWindowDuration),
+                    currentInstant.plus(retentionWindowDuration))) {
+                while (resultIterator != null && resultIterator.hasNext()) {
+                    var currentKeyValue = resultIterator.next();
+                    if (identifier.equals(currentKeyValue.value)) {
+                        return;
+                    }
+                }
+            }
+            // First time we see this record, store entry in the window store and forward the record to the output
+            dedupWindowStore.put(identifier, identifier, message.timestamp());
+            processorContext.forward(ProcessingResult.wrapRecordSuccess(message));
+        } catch (Exception e) {
+            processorContext.forward(ProcessingResult.wrapRecordFailure(
+                    e,
+                    message,
+                    "Could not figure out what to do with the current payload: "
+                            + "An unlikely error occurred during deduplication transform"));
+        }
+    }
+
+    /**
+     * Build an identifier for the record based on the headers and the keys provided.
+     *
+     * @param headers The headers of the record
+     * @return The built identifier
+     */
+    private String buildIdentifier(Headers headers) {
+        return deduplicationHeadersList.stream()
+                .map(key -> getHeader(headers, key))
+                .collect(Collectors.joining("#"));
+    }
+
+    /**
+     * Get the header value for a given key
+     *
+     * @param headers headers of the record
+     * @param key the key to look for in the headers
+     * @return The header value for the given key, or an empty string if the header is not present or has no value.
+     */
+    private static String getHeader(Headers headers, String key) {
+        Header header = headers.lastHeader(key);
+        if (header == null || header.value() == null) {
+            return StringUtils.EMPTY;
+        }
+        String value = new String(header.value(), StandardCharsets.UTF_8);
+        return StringUtils.defaultString(value);
+    }
+}
---
+++ kstreamplify-core/src/test/java/com/michelin/kstreamplify/deduplication/DedupHeadersProcessorTest.java
@@ -0,0 +1,126 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.michelin.kstreamplify.deduplication;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.michelin.kstreamplify.avro.KafkaError;
+import com.michelin.kstreamplify.error.ProcessingResult;
+import java.time.Duration;
+import java.util.List;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.processor.api.ProcessorContext;
+import org.apache.kafka.streams.processor.api.Record;
+import org.apache.kafka.streams.state.WindowStore;
+import org.apache.kafka.streams.state.WindowStoreIterator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class DedupHeadersProcessorTest {
+
+    private DedupHeadersProcessor<KafkaError> processor;
+
+    @Mock
+    private ProcessorContext<String, ProcessingResult<KafkaError, KafkaError>> context;
+
+    @Mock
+    private WindowStore<String, String> windowStore;
+
+    @Mock
+    private WindowStoreIterator<String> windowStoreIterator;
+
+    @BeforeEach
+    void setUp() {
+        // Create an instance of DedupWithHeadersProcessor for testing
+        processor = new DedupHeadersProcessor<>("testStore", Duration.ofHours(1), List.of("header-1", "header-3"));
+        // Stub the context.getStateStore method to return the mock store
+        when(context.getStateStore("testStore")).thenReturn(windowStore);
+
+        processor.init(context);
+    }
+
+    @Test
+    void shouldProcessNewRecord() {
+        final KafkaError kafkaError = new KafkaError();
+        final Record<String, KafkaError> message = new Record<>("key", kafkaError, 0);
+        message.headers().add("header-1", "value-1".getBytes());
+        message.headers().add("header-2", "value-2".getBytes());
+        message.headers().add("header-3", "value-3".getBytes());
+        processor.process(message);
+
+        verify(windowStore).put("value-1#value-3", "value-1#value-3", message.timestamp());
+        verify(context).forward(argThat(arg -> arg.value().getValue().equals(message.value())));
+    }
+
+    @Test
+    void shouldProcessDuplicate() {
+        final KafkaError kafkaError = new KafkaError();
+        final Record<String, KafkaError> message = new Record<>("key", kafkaError, 0);
+        message.headers().add("header-1", "value-1".getBytes());
+        message.headers().add("header-2", "value-2".getBytes());
+        message.headers().add("header-3", "value-3".getBytes());
+
+        // Simulate hasNext() returning true once and then false
+        when(windowStoreIterator.hasNext()).thenReturn(true);
+
+        // Simulate the condition to trigger the return statement
+        when(windowStoreIterator.next()).thenReturn(KeyValue.pair(0L, "value-1#value-3"));
+
+        // Simulate the backwardFetch() method returning the mocked ResultIterator
+        when(windowStore.backwardFetch(any(), any(), any())).thenReturn(windowStoreIterator);
+
+        // Call the process method
+        processor.process(message);
+
+        verify(windowStore, never()).put(anyString(), any(), anyLong());
+        verify(context, never()).forward(any());
+    }
+
+    @Test
+    void shouldThrowException() {
+        Record<String, KafkaError> message = new Record<>("key", new KafkaError(), 0L);
+        message.headers().add("header-1", "value-1".getBytes());
+        message.headers().add("header-2", "value-2".getBytes());
+        message.headers().add("header-3", "value-3".getBytes());
+
+        when(windowStore.backwardFetch(any(), any(), any()))
+                .thenReturn(null)
+                .thenThrow(new RuntimeException("Exception..."));
+        doThrow(new RuntimeException("Exception...")).when(windowStore).put(anyString(), any(), anyLong());
+
+        processor.process(message);
+
+        verify(context).forward(argThat(arg -> arg.value()
+                .getError()
+                .getContextMessage()
+                .equals("Could not figure out what to do with the current payload: "
+                        + "An unlikely error occurred during deduplication transform")));
+    }
+}
